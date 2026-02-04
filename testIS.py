056590664eebdb7a434af06117a09b8ba6692c4c
Haya <aminovhaya@gmail.com>
2026-02-04 11:09:36 +0200

    first commit

---
+++ testIS.py
@@ -0,0 +1,7 @@
+import os
+
+dir = "d:\\Toolbox\\"
+
+apps = ["UserManagement.exe", "ConfigurationEditor.exe", "DatabaseChecking.exe", "DatabasePurging.exe", "DataScience.exe", 
+        "FormulaEditor.exe", "HierarchyManager.exe", "ToolboxDesktop.exe", "SystemBuilder.exe", "Planner.exe", "ConfigurationEditor.exe"
+        "Maintenance.exe", "LogAnalyzer.exe", "LoadData.exe", "IntegrationBuilder.exe", "BAR.exe", "ActiveUsers.exe"]   
