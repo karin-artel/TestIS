6dc2de983cf5e0ba66f0d917ea2a0ba9a79e8b7d
Haya <aminovhaya@gmail.com>
2026-02-04 14:04:10 +0200

    apps exe

--- testIS.py
+++ testIS.py
@@ -1,7 +1,12 @@
 import os
 
-dir = "d:\\Toolbox\\"
+dir = "d:\\Toolbox AP\\"
 
 apps = ["UserManagement.exe", "ConfigurationEditor.exe", "DatabaseChecking.exe", "DatabasePurging.exe", "DataScience.exe", 
-        "FormulaEditor.exe", "HierarchyManager.exe", "ToolboxDesktop.exe", "SystemBuilder.exe", "Planner.exe", "ConfigurationEditor.exe"
+        "FormulaEditor.exe", "HierarchyManager.exe", "ToolboxDesktop.exe", "SystemBuilder.exe", "Planner.exe", "ConfigurationEditor.exe",
         "Maintenance.exe", "LogAnalyzer.exe", "LoadData.exe", "IntegrationBuilder.exe", "BAR.exe", "ActiveUsers.exe"]   
+
+for app in apps:
+        app_path = os.path.join(dir, app)
+        if not os.path.exists(app_path):
+                print(f"{app} not found \n")
